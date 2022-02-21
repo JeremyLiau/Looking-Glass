df3da8fe4c0f936a04e99469796308b1151d20c7
Jeremy Liau <jwl265@mail.usask.ca>
2022-02-20 20:14:55 -0600

    Created wall of death and looking glass

---
+++ Scripts/LookingGlass.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	position = get_global_mouse_position()
+	if Input.is_action_just_pressed("left_mouse"):
+		if scale == Vector2(1, 1):
+			scale = Vector2(0.1, 0.1)
+		else:
+			scale = Vector2(1, 1)
