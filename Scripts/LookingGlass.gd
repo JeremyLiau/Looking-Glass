f685e3578d24ed506137bc82733df4cf902d6290
Jeremy Liau <jwl265@mail.usask.ca>
2022-02-20 21:01:30 -0600

    Looking glass collision shape disabled when in icon mode

--- Scripts/LookingGlass.gd
+++ Scripts/LookingGlass.gd
@@ -17,5 +17,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("left_mouse"):
 		if scale == standardScale:
 			scale = iconScale
+			$CollisionShape2D.disabled = true
 		else:
 			scale = standardScale
+			$CollisionShape2D.disabled = false
