64ba356b8fa4c215e39ddf7078a05bd90e0a0be3
Jeremy Liau <jwl265@mail.usask.ca>
2022-02-21 00:17:42 -0600

    Regular, Hidden and Illusory floor switches created

--- Scripts/HiddenFloorSwitch.gd
+++ Scripts/HiddenFloorSwitch.gd
@@ -2,16 +2,20 @@ extends Node2D
 
 onready var tween = get_node("Tween")
 
+func _on_Area2D_body_entered(body):
+	if body.get_name() == "Player":
+		print("Hidden floor switch stepped on")
+		
 func _on_Area2D_area_entered(area):
 	if area.get_name() == "LookingGlass":
 		tween.interpolate_property($Sprite, "modulate",
-		Color(1,1,1,1), Color(1,1,1,0.2), .2,
+		Color(1,1,1,0), Color(1,1,1,1), .2,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.start()
 
 func _on_Area2D_area_exited(area):
 	if area.get_name() == "LookingGlass":
 		tween.interpolate_property($Sprite, "modulate",
-		Color(1,1,1,0.2), Color(1,1,1,1), .2,
+		Color(1,1,1,1), Color(1,1,1,0), .2,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.start()
--- Scripts/IllusoryFloorSwitch.gd
+++ Scripts/IllusoryFloorSwitch.gd
@@ -2,16 +2,21 @@ extends Node2D
 
 onready var tween = get_node("Tween")
 
+func _on_Area2D_body_entered(body):
+	if body.get_name() == "Player":
+		print("Illusory floor switch stepped on")
+		# Use disintegration effect to make the switch disappear when stepped on
+		
 func _on_Area2D_area_entered(area):
 	if area.get_name() == "LookingGlass":
 		tween.interpolate_property($Sprite, "modulate",
-		Color(1,1,1,1), Color(1,1,1,0.2), .2,
+		Color(1,1,1,1), Color(1,1,1,0.5), .2,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.start()
 
 func _on_Area2D_area_exited(area):
 	if area.get_name() == "LookingGlass":
 		tween.interpolate_property($Sprite, "modulate",
-		Color(1,1,1,0.2), Color(1,1,1,1), .2,
+		Color(1,1,1,0.5), Color(1,1,1,1), .2,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.start()
