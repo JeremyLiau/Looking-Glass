8dbfea5238a5401aea06129544c87925033ec526
Jeremy Liau <jwl265@mail.usask.ca>
2022-02-23 00:09:17 -0600

    First iteration of the floor switch mechanic

--- Scripts/FloorSwitch.gd
+++ Scripts/FloorSwitch.gd
@@ -1,7 +1,39 @@
 extends Node2D
 
 var isReal = true
+onready var anim = $AnimatedSprite
+var activated = false
+
+onready var raycastNorth = $RayCastNorth
+var northDir = Vector2(0, -1)
+onready var raycastEast = $RayCastEast
+var eastDir = Vector2(1, 0)
+onready var raycastSouth = $RayCastSouth
+var southDir = Vector2(0, 1)
+onready var raycastWest = $RayCastWest
+var westDir = Vector2(-1, 0)
+
+var interactDist : int = 32
 
 func _on_Area2D_body_entered(body):
 	if body.get_name() == "Player" && isReal:
-		print("Switch pressed")
+		if(!activated):
+			activated = true
+			anim.play("activated")
+		else:
+			activated = false
+			anim.play("deactivated")
+
+func try_interact(raycast, dir):
+	raycast.cast_to = dir * interactDist
+	if raycast.is_colliding():
+		print(raycast.get_collider())
+		if raycast.get_collider().has_method("on_interact"):
+			raycast.get_collider().on_interact()
+
+
+func _on_AnimatedSprite_animation_finished():
+	try_interact(raycastNorth, northDir)
+	try_interact(raycastEast, eastDir)
+	try_interact(raycastSouth, southDir)
+	try_interact(raycastWest, westDir)
