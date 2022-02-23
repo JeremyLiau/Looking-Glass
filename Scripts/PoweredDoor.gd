fef1138cc32096fb6f485b8e631bf0b236f61134
Jeremy Liau <jwl265@mail.usask.ca>
2022-02-23 02:06:37 -0600

    Powered door works with power tile lines!

--- Scripts/PoweredDoor.gd
+++ Scripts/PoweredDoor.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 onready var anim = $AnimatedSprite
 var activated = false
-onready var collisionShape = $CollisionShape2D
+onready var collisionShape = $StaticBody2D/CollisionShape2D
+
 
 func on_interact():
 	if(!activated):
@@ -10,11 +11,13 @@ func on_interact():
 		anim.play("activated")
 	else:
 		activated = false
+		collisionShape.disabled = false
 		anim.play("deactivated")
 
 
 func _on_AnimatedSprite_animation_finished():
+	pass
 	if(activated):
 		collisionShape.disabled = true
-	else:
-		collisionShape.disabled = false
+	#else:
+	#	collisionShape.disabled = false
