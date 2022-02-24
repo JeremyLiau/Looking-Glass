301ba9a3ca7b6b420c03058a2b72cdb1a24289c9
Jeremy Liau <jwl265@mail.usask.ca>
2022-02-23 22:25:57 -0600

    Created visual clarity for when player stands behind a powered door
    Includes all interactions with player and looking glass fade effects

--- Scripts/IllusoryPoweredDoor.gd
+++ Scripts/IllusoryPoweredDoor.gd
@@ -5,6 +5,8 @@ var activated = false
 onready var collisionShape = $StaticBody2D/CollisionShape2D
 onready var tween = get_node("Tween")
 var illusory = true
+var lookingGlassed = false #Terrible name, but essentially means that the looking glass is hovering over the object. This is checked to make sure that the lookingGlass fade effect takes precedence over the half-fade when the player stands behind an object
+var playerBehind = false #One last check to fix visual clarity. Ensure that the fade remains at half-fade if the looking glass is removed while the player stands behind the object.
 
 func on_interact():
 	if(!illusory):
@@ -23,6 +25,7 @@ func _on_AnimatedSprite_animation_finished():
 func _on_FadeArea_area_entered(area):
 	if(illusory):
 		if area.get_name() == "LookingGlass":
+			lookingGlassed = true
 			tween.interpolate_property(anim, "modulate",
 			Color(1,1,1,1), Color(1,1,1,0), .2,
 			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
@@ -31,13 +34,20 @@ func _on_FadeArea_area_entered(area):
 func _on_FadeArea_area_exited(area):
 	if(illusory):
 		if area.get_name() == "LookingGlass":
-			tween.interpolate_property(anim, "modulate",
-			Color(1,1,1,0), Color(1,1,1,1), .2,
-			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			lookingGlassed = false
+			if(playerBehind):
+				tween.interpolate_property(anim, "modulate",
+					Color(1,1,1,0), Color(1,1,1,0.5), .2,
+				Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			else:
+				tween.interpolate_property(anim, "modulate",
+					Color(1,1,1,0), Color(1,1,1,1), .2,
+				Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 		tween.start()
 
 func _on_FadeArea_body_entered(body):
-	if(illusory):
+	playerBehind = true
+	if(illusory and !lookingGlassed):
 		if body.get_name() == "Player":
 			tween.interpolate_property(anim, "modulate",
 			Color(1,1,1,1), Color(1,1,1,0.5), .2,
@@ -45,7 +55,8 @@ func _on_FadeArea_body_entered(body):
 		tween.start()
 
 func _on_FadeArea_body_exited(body):
-	if(illusory):
+	playerBehind = false
+	if(illusory and !lookingGlassed):
 		if body.get_name() == "Player":
 			tween.interpolate_property(anim, "modulate",
 			Color(1,1,1,0.5), Color(1,1,1,1), .2,
--- Scripts/PoweredDoor.gd
+++ Scripts/PoweredDoor.gd
@@ -3,7 +3,7 @@ extends Node2D
 onready var anim = $AnimatedSprite
 var activated = false
 onready var collisionShape = $StaticBody2D/CollisionShape2D
-
+onready var tween = get_node("Tween")
 
 func on_interact():
 	if(!activated):
@@ -16,8 +16,22 @@ func on_interact():
 
 
 func _on_AnimatedSprite_animation_finished():
-	pass
 	if(activated):
 		collisionShape.disabled = true
-	#else:
-	#	collisionShape.disabled = false
+
+
+func _on_FadeArea_body_entered(body):
+	if(!activated):
+		if body.get_name() == "Player":
+			tween.interpolate_property(anim, "modulate",
+			Color(1,1,1,1), Color(1,1,1,0.5), .2,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		tween.start()
+
+func _on_FadeArea_body_exited(body):
+	if(!activated):
+		if body.get_name() == "Player":
+			tween.interpolate_property(anim, "modulate",
+			Color(1,1,1,0.5), Color(1,1,1,1), .2,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		tween.start()
