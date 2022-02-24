0386032c26d45aaa1fc9fa683592dd76ebfd2042
Jeremy Liau <jwl265@mail.usask.ca>
2022-02-24 07:01:36 -0600

    Created first iteration of lever mechanic

--- Scripts/Lever.gd
+++ Scripts/Lever.gd
@@ -1,9 +1,21 @@
 extends StaticBody2D
 
 onready var anim = $AnimatedSprite
+var illusionToggleMembers
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
 
 func on_interact ():
 	if(anim.frame == 0):
 		anim.frame = 1
+		illusionToggleMembers = get_tree().get_nodes_in_group("Toggle")
+		for member in illusionToggleMembers:
+			member.illusion_toggle()
 	else:
 		anim.frame = 0
+		illusionToggleMembers = get_tree().get_nodes_in_group("Toggle")
+		for member in illusionToggleMembers:
+			member.illusion_toggle()
+
+#Find all objects in the tree that have a function "IllusionToggle"
