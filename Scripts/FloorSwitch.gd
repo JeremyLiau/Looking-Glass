cdca06e667a0cf49de0a563d6da4297a00f279bf
Jeremy Liau <jwl265@mail.usask.ca>
2022-02-24 09:24:42 -0600

    Added toggle state to floor switches and fixed visual clarity
    Visual clarity change: When the looking glass is hovered over mid-toggle switches, the visual will remain correct for its state.

--- Scripts/FloorSwitch.gd
+++ Scripts/FloorSwitch.gd
@@ -1,6 +1,4 @@
 extends Node2D
-
-var isReal = true
 onready var anim = $AnimatedSprite
 var activated = false
 
@@ -15,8 +13,12 @@ var westDir = Vector2(-1, 0)
 
 var interactDist : int = 32
 
+export var illusory = true
+onready var tween = get_node("Tween")
+var lookingGlassed = false #Terrible name, but essentially means that the looking glass is hovering over the object. This is used in the illusion toggle to retain visual when hovered with looking glass while toggling
+
 func _on_Area2D_body_entered(body):
-	if body.get_name() == "Player" && isReal:
+	if body.get_name() == "Player":
 		if(!activated):
 			activated = true
 			anim.play("activated")
@@ -30,9 +32,44 @@ func try_interact(raycast, dir):
 		if raycast.get_collider().has_method("on_interact"):
 			raycast.get_collider().on_interact()
 
-
 func _on_AnimatedSprite_animation_finished():
 	try_interact(raycastNorth, northDir)
 	try_interact(raycastEast, eastDir)
 	try_interact(raycastSouth, southDir)
 	try_interact(raycastWest, westDir)
+
+func _on_FadeArea_area_entered(area):
+	if area.get_name() == "LookingGlass":
+		lookingGlassed = true
+		if(illusory):
+			tweenEffect(1, 0)
+		else:
+			tweenEffect(0, 1)
+
+func _on_FadeArea_area_exited(area):
+	if area.get_name() == "LookingGlass":
+		lookingGlassed = false
+		if(illusory):
+			tweenEffect(0, 1)
+		else:
+			tweenEffect(1, 0)
+
+func illusion_toggle():
+	if(illusory):
+		illusory = false
+		if(!lookingGlassed):
+			tweenEffect(1, 0)
+		else:
+			tweenEffect(0, 1)
+	else:
+		illusory = true
+		if(!lookingGlassed):
+			tweenEffect(0, 1)
+		else:
+			tweenEffect(1, 0)
+
+func tweenEffect(from, to):
+	tween.interpolate_property(anim, "modulate",
+	Color(1,1,1,from), Color(1,1,1,to), .2,
+	Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
