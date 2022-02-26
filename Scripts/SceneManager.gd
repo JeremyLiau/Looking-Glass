3e328ce156d1cd0da2b001ef5ab5af0a18ac64b3
Jeremy Liau <jwl265@mail.usask.ca>
2022-02-26 13:11:34 -0600

    Created end-game splash

--- Scripts/SceneManager.gd
+++ Scripts/SceneManager.gd
@@ -6,8 +6,9 @@ const LEVEL2 = preload("res://Scenes/Levels/Level2.tscn")
 const LEVEL3 = preload("res://Scenes/Levels/Level3.tscn")
 const LEVEL4 = preload("res://Scenes/Levels/Level4.tscn")
 const LEVEL5 = preload("res://Scenes/Levels/Level5.tscn")
+const EXITSPLASH = preload("res://Scenes/Levels/ExitSplash.tscn")
 
-var levelOrder = [LEVEL1, LEVEL2, LEVEL3, LEVEL4, LEVEL5]
+var levelOrder = [LEVEL1, LEVEL2, LEVEL3, LEVEL4, LEVEL5, EXITSPLASH]
 
 func _on_TransitionScreen_transitioned():
 	$CurrentScene.get_child(0).queue_free()
