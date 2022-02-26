35e95e422b800ed7fc001b49fe0363887d907755
Jeremy Liau <jwl265@mail.usask.ca>
2022-02-26 08:42:32 -0600

    Improved looking glass sprite and added shine animation

--- Scripts/LookingGlass.gd
+++ Scripts/LookingGlass.gd
@@ -1,17 +1,17 @@
 extends Area2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var standardScale = Vector2(1, 1)
 var iconScale = Vector2(0.1, 0.1)
 
-# Called when the node enters the scene tree for the first time.
+onready var anim = $AnimatedSprite
+var shineTimer = Timer.new()
+
 func _ready():
-	pass # Replace with function body.
+	shineTimer.set_wait_time(20)
+	shineTimer.connect("timeout", self, "shineAnimation")
+	add_child(shineTimer)
+	shineTimer.start()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
 	position = get_global_mouse_position()
 	if Input.is_action_just_pressed("left_mouse"):
@@ -21,3 +21,9 @@ func _physics_process(_delta):
 		else:
 			scale = standardScale
 			$CollisionShape2D.disabled = false
+
+func shineAnimation():
+	anim.play("shine")
+
+func _on_AnimatedSprite_animation_finished():
+	anim.play("default")
